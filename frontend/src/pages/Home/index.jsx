8b2732bce1f360a1ba03350f5a4ed76bf16bd183
Noslig <gilszon@gmail.com>
2021-01-07 18:54:53 -0300

    Front

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 
 import Animation from '../../components/Animation';
 import CardCourse from '../../components/CardCourse';
+import CardCategory from '../../components/CardCategory';
 
 import './styles.css';
 
 import yoda from '../../assets/images/characters/yoda.png';
 import separator from '../../assets/images/separator.svg';
 import courseReactRedux from '../../assets/images/courses/react-redux.jpg';
-import coursePHP from '../../assets/images/courses/php.jpg';
-import courseFlutter from '../../assets/images/courses/flutter.jpg';
-import CardCategory from '../../components/CardCategory';
-
-
+import coursePHP from '../../assets/images/courses/php.jpg'
+import courseFlutter from '../../assets/images/courses/flutter.jpg'
+import markRight from '../../assets/images/mark-right.svg';
+import markLeft from '../../assets/images/mark-left.svg';
+import userReviewImage from '../../assets/images/users/user.png';
 
 const Home = () => {
     return (
         <>
-            {/* <Animation/> */}
+            <Animation />
             <div className="section-1">
-                <div className="container">
+                <div className="box">
                     <header className="header">
                         <h2>Luke Code</h2>
                         <nav>
@@ -29,7 +30,7 @@ const Home = () => {
                             <button>
                                 INSCREVA-SE
                             </button>
-                            <button className="btn-menu"> 
+                            <button className="btn-menu">
                                 <i className="fas fa-bars"></i>
                             </button>
                         </nav>
@@ -37,7 +38,7 @@ const Home = () => {
                     <main>
                         <section>
                             <h1>Se torne um mestre <br/> da programação!</h1>
-                            <img src={separator} alt="Separator" />
+                            <img src={separator} alt="" />
                             <p>
                                 De o próximo passo para o seu futuro, <br />
                                 que a força esteja com você.
@@ -49,57 +50,74 @@ const Home = () => {
                     </main>
                 </div>
             </div>
+            
             <div className="section-2">
-                <div className="container">
-                    <h2>+3 Cursos Completo</h2>
-                    <p>Na Luke code, você tem acesso a 3 cursos gratuitos, basta se cadastrar na plataforma.</p>
+                <div className="box">
+                    <h2>+3 Cursos Completos</h2>
+                    <p>
+                        Na Luke code, você tem acesso a 3 cursos gratuitos, basta se cadastrar na plataforma.
+                    </p>
                     <section className="card-container">
-                        <CardCourse image={courseReactRedux}> 
-                            Desenvolvimento front-end <br/> React e Redux
+                        <CardCourse image={courseReactRedux} description="Desenvolvimento front-end React e Redux" > 
+                           Desenvolvimento front-end <br /> React e Redux
                         </CardCourse>
-                            
-                        <CardCourse image={coursePHP} description={"PHP 7"}>
-                            Desenvolvimento em PHP
-                        </CardCourse> 
-                        <CardCourse image={courseFlutter} description={"Flutter"}>
-                            Desenvolvimento em Flutter
-                        </CardCourse>                      
+                        <CardCourse image={coursePHP} description="PHP 7 - Trabalhando com PSRs">
+                            PHP 7 - Trabalhando com PSRs
+                        </CardCourse>
+                        <CardCourse image={courseFlutter} description="Desenvolvimento IOS e Android com Flutter" > 
+                            Desenvolvimento IOS e Android <br /> com Flutter
+                        </CardCourse>
                     </section>
                     <button>
-                            INSCREVA-SE
-                    </button>
-                    <button className="btn-menu">
-                            MENU
+                        INSCREVA-SE
                     </button>
                 </div>
-
-                    
             </div>
+
             <div className="section-3">
-                <div className="container">
+                <div className="box">
                     <h2>O que oferecemos para você</h2>
-                        
-
-                         <section className="categories-container">  
-                            <CardCategory image="https://www.flaticon.com/svg/static/icons/svg/622/622397.svg" title="BACK-END" courses="22 cursos" color="red"/>
-                            <CardCategory image="https://www.flaticon.com/svg/static/icons/svg/689/689401.svg" title="BANCO DE DADOS" courses="8 cursos" color="yellow"/>
-                            <CardCategory image="https://www.flaticon.com/svg/static/icons/svg/388/388531.svg" title="SEGURANÇA" courses="12 cursos" color="green"/>                   
-                            <CardCategory image="https://www.flaticon.com/svg/static/icons/svg/545/545245.svg"  title="MOBILE" courses="16 cursos" color="blue"/>
-                            <CardCategory image="https://www.flaticon.com/svg/static/icons/svg/781/781683.svg" title="FRONT-END" courses="18 cursos" color="#c3c3c3c3"/>
-                            <CardCategory image="https://www.flaticon.com/svg/static/icons/svg/622/622340.svg" title="CLOUDING COMPUTING" courses="14 cursos" color="orange"/>
-
-                           
-                         </section>
-
-                         <footer>
-                            <div className="box">
-                                &copy; {new Date().getFullYear()} - Luke Code - Alguns direitos reservados.
-                            </div>
-                         </footer>
+                    <section className="categories-container">
+                        <CardCategory title="BACK-END" courses="22 cursos" color="red" />
+                        <CardCategory title="BANCO DE DADOS" courses="2 cursos" color="yellow" />
+                        <CardCategory title="SEGURANÇA" courses="22 cursos" color="purple" />
+                        <CardCategory title="MOBILE" courses="17 cursos" color="blue" />
+                        <CardCategory title="FRONT-END" courses="12 cursos" color="green" />
+                        <CardCategory title="CLOUD-COMPUTING" courses="8 cursos" color="orange" />
+                    </section>
                 </div>
             </div>
+
+            <div className="section-4">
+                <div className="box">
+                    <h2>Depoimentos</h2>
+                    <div className="image-review">
+                        <img src={userReviewImage} alt="Joaquim" />
+                    </div>
+
+                    <div className="review">
+                        <img src={markLeft} alt="" />
+                        <p>
+                            Professor extremamente didático e com amplos conhecimentos <br />
+                            e o curso tem conteúdo que permite exercitar o que é ensinado!
+                        </p>
+                        <img src={markRight} alt="" />
+                    </div>
+
+                    <div className="user-review">
+                        <h4>
+                            Joaquim
+                        </h4>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                <div className="box">
+                    &copy; { new Date().getFullYear() } - Luke Code - Alguns direitos reservados
+                </div>
+            </footer>
         </>
     );
 }
-
 export default Home;
